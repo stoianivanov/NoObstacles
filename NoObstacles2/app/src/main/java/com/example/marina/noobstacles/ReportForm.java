713f67957bc9f292c9d3d3cad5606d915cbf94e4
Stoian <stoian_ivanov_ivanov@abv.bg>
2016-05-29 01:45:32 +0300

    add click method

--- NoObstacles2/app/src/main/java/com/example/marina/noobstacles/ReportForm.java
+++ NoObstacles2/app/src/main/java/com/example/marina/noobstacles/ReportForm.java
@@ -8,8 +8,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class ReportForm extends Activity {
 
@@ -22,12 +24,16 @@ public class ReportForm extends Activity {
             "Път в ремонт",
             "Временно препятствие",
             "Друго"};
+    private String lng;
+    private String lat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_form);
 
         Bundle bundle = getIntent().getExtras();
+        lat = bundle.getString("Latitude");
+        lng = bundle.getString("Longitude");
         Log.d("Log", bundle.getString("Latitude"));
         Log.d("Log", bundle.getString("Longitude"));
 
@@ -35,6 +41,16 @@ public class ReportForm extends Activity {
 
         ListView listView = (ListView) findViewById(R.id.problems_list);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+               
+
+                String msg = new String(lat + "|" + lng);
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
